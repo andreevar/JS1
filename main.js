9b54b3893151cee5be8ed9fffe2208809917535b
Andreeva Regina <andreeva.rr@mail.ru>
2025-03-10 19:59:44 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,25 @@
+alert("Hello World") 
+console.log("Hello World")
+
+let myFirstVariable = 50
+let age = 60
+
+console.log(myFirstVariable)
+console.log(age)
+//Тип данных number - хранит в себе численные зн. (целые и числа с плав. точкой)
+
+let name = 'Regina'
+alert(name)
+alert(`Мне ${age} лет`) //${} - внедрить переменную в текст
+
+let boolValue = true
+console.log(boolValue)
+
+console.log(typeof(boolValue)) //увидеть тип данных
+
+//let result = prompt("Как дела?") //строка для ввода
+//let result = confirm("Как дела?") //для булевого значения
+
+let names = prompt ("Ваше имя?", "Regina")
+alert(names)
+console.log(names)
