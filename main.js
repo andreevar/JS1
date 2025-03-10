4dfa2513b68aed59e9c036bd6fd15c72615d84ac
Andreeva Regina <andreeva.rr@mail.ru>
2025-03-10 21:02:58 +0300

    first commit

--- main.js
+++ main.js
@@ -1,25 +1,60 @@
-alert("Hello World") 
-console.log("Hello World")
+// alert("Hello World") 
+// console.log("Hello World")
 
-let myFirstVariable = 50
-let age = 60
+// let myFirstVariable = 50
+// let age = 60
 
-console.log(myFirstVariable)
-console.log(age)
-//Тип данных number - хранит в себе численные зн. (целые и числа с плав. точкой)
+// console.log(myFirstVariable)
+// console.log(age)
+// //Тип данных number - хранит в себе численные зн. (целые и числа с плав. точкой)
 
-let name = 'Regina'
-alert(name)
-alert(`Мне ${age} лет`) //${} - внедрить переменную в текст
+// let name = 'Regina'
+// alert(name)
+// alert(`Мне ${age} лет`) //${} - внедрить переменную в текст
 
-let boolValue = true
-console.log(boolValue)
+// let boolValue = true
+// console.log(boolValue)
 
-console.log(typeof(boolValue)) //увидеть тип данных
+// console.log(typeof(boolValue)) //увидеть тип данных
 
-//let result = prompt("Как дела?") //строка для ввода
-//let result = confirm("Как дела?") //для булевого значения
+// //let result = prompt("Как дела?") //строка для ввода
+// //let result = confirm("Как дела?") //для булевого значения
 
-let names = prompt ("Ваше имя?", "Regina")
-alert(names)
-console.log(names)
+// let names = prompt ("Ваше имя?", "Regina")
+// alert(names)
+// console.log(names)
+
+//Преобразование типов
+
+// let value = true
+// console.log(typeof(value))
+// alert(value)
+
+// let transformation = Number(value)
+// console.log(typeof(transformation))
+// alert(transformation)
+
+// let value = 64
+// let value1 = 4
+// let result = value / value1
+// console.log(result)
+
+// // * - умножить
+// // ** - квадрат числа, степень
+// // / - разделить
+
+// let str = "одно яблоко" + "одно яблоко"
+// console.log (str)
+// //конкатенация
+
+//инкремент ++ -увеличивает на 1, --уменьшает на 1
+// let counter = 2
+// counter++
+// counter--
+// console.log(counter)
+
+let number1 = prompt("число1","")
+let number2 = prompt("число2","")
+let result = number1 * number2
+alert("ответ: ${result}")
+console.log(result)
